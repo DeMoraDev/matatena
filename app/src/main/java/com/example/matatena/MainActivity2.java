17baf5e3c357b14e0e640496c4d02d9d964c0e2a
Viejo <demoraserrano20@gmail.com>
2024-04-08 18:20:41 +0200

    Implementación de todos las imágenes para cada boton y sus listeners

--- app/src/main/java/com/example/matatena/MainActivity2.java
+++ app/src/main/java/com/example/matatena/MainActivity2.java
@@ -30,6 +30,13 @@ public class MainActivity2 extends AppCompatActivity {
 
     private ImageView dice_3_0;
     private ImageView dice_3_1;
+    private ImageView dice_3_2;
+    private ImageView dice_4_0;
+    private ImageView dice_4_1;
+    private ImageView dice_4_2;
+    private ImageView dice_5_0;
+    private ImageView dice_5_1;
+    private ImageView dice_5_2;
 
     int[][] tablero;
     int tirada;
@@ -40,7 +47,7 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        roller1 = findViewById(R.id.roller1);
+        roller1 = findViewById(R.id.roller_Jugador);
         rollerimage = findViewById(R.id.rollerimage);
 
         hueco_5_1 = findViewById(R.id.hueco_5_1);
@@ -53,13 +60,20 @@ public class MainActivity2 extends AppCompatActivity {
         hueco_3_0 = findViewById(R.id.hueco_3_0);
         hueco_3_2 = findViewById(R.id.hueco_3_2);
 
-        puntosJugador = findViewById(R.id.puntos1);
+        puntosJugador = findViewById(R.id.puntos_Jugador);
         puntos_Jugador_columna_0 = findViewById(R.id.ptsfila1);
         puntos_Jugador_columna_1 = findViewById(R.id.ptsfila2);
         puntos_Jugador_columna_2 = findViewById(R.id.ptsfila3);
 
         dice_3_0 = findViewById(R.id.dice_3_0);
         dice_3_1 = findViewById(R.id.dice_3_1);
+        dice_3_2 = findViewById(R.id.dice_3_2);
+        dice_4_0 = findViewById(R.id.dice_4_0);
+        dice_4_1 = findViewById(R.id.dice_4_1);
+        dice_4_2 = findViewById(R.id.dice_4_2);
+        dice_5_0 = findViewById(R.id.dice_5_0);
+        dice_5_1 = findViewById(R.id.dice_5_1);
+        dice_5_2 = findViewById(R.id.dice_5_2);
 
         tablero = new int[6][3];
 
@@ -69,18 +83,56 @@ public class MainActivity2 extends AppCompatActivity {
 
         hueco_3_0.setOnClickListener(v -> {
             if (rolleado) {
-                {
-                    tablero[3][0] = tirada;
-                    dice_3_0.setImageResource(getDiceDrawable(tirada));
-                }
+                tablero[3][0] = tirada;
+                dice_3_0.setImageResource(getDiceDrawable(tirada));
             }
         });
         hueco_3_1.setOnClickListener(v -> {
             if (rolleado) {
-                {
-                    tablero[3][1] = tirada;
-                    dice_3_1.setImageResource(getDiceDrawable(tirada));
-                }
+                tablero[3][1] = tirada;
+                dice_3_1.setImageResource(getDiceDrawable(tirada));
+            }
+        });
+        hueco_3_2.setOnClickListener(v -> {
+            if (rolleado) {
+                tablero[3][2] = tirada;
+                dice_3_2.setImageResource(getDiceDrawable(tirada));
+            }
+        });
+        hueco_4_0.setOnClickListener(v -> {
+            if (rolleado) {
+                tablero[4][0] = tirada;
+                dice_4_0.setImageResource(getDiceDrawable(tirada));
+            }
+        });
+        hueco_4_1.setOnClickListener(v -> {
+            if (rolleado) {
+                tablero[4][1] = tirada;
+                dice_4_1.setImageResource(getDiceDrawable(tirada));
+            }
+        });
+        hueco_4_2.setOnClickListener(v -> {
+            if (rolleado) {
+                tablero[4][2] = tirada;
+                dice_4_2.setImageResource(getDiceDrawable(tirada));
+            }
+        });
+        hueco_5_0.setOnClickListener(v -> {
+            if (rolleado) {
+                tablero[5][0] = tirada;
+                dice_5_0.setImageResource(getDiceDrawable(tirada));
+            }
+        });
+        hueco_5_1.setOnClickListener(v -> {
+            if (rolleado) {
+                tablero[5][1] = tirada;
+                dice_5_1.setImageResource(getDiceDrawable(tirada));
+            }
+        });
+        hueco_5_2.setOnClickListener(v -> {
+            if (rolleado) {
+                tablero[5][2] = tirada;
+                dice_5_2.setImageResource(getDiceDrawable(tirada));
             }
         });
     }
