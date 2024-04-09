ebeca2a9294186dac8863a2593f87f2ff4427097
Viejo <demoraserrano20@gmail.com>
2024-04-09 15:23:03 +0200

    Añadidas imagenes doubles

--- app/src/main/java/com/example/matatena/MainActivity2.java
+++ app/src/main/java/com/example/matatena/MainActivity2.java
@@ -85,10 +85,12 @@ public class MainActivity2 extends AppCompatActivity {
         });
 
         hueco_3_0.setOnClickListener(v -> {
-            if (rolleado) {
-                tablero[3][0] = tirada;
-                dice_3_0.setImageResource(getDiceDrawable(tirada));
-                updateCounter();
+            if (tablero[3][0] == 0) {
+                if (rolleado) {
+                    tablero[3][0] = tirada;
+                    dice_3_0.setImageResource(getDiceDrawable(tirada));
+                    updateCounter();
+                }
             }
         });
         hueco_3_1.setOnClickListener(v -> {
@@ -175,7 +177,47 @@ public class MainActivity2 extends AppCompatActivity {
         }
     }
 
+    private int getDiceDrawableDouble(int tirada) {
+        switch (tirada) {
+            case 1:
+                return R.drawable.dice1double;
+            case 2:
+                return R.drawable.dice2double;
+            case 3:
+                return R.drawable.dice3double;
+            case 4:
+                return R.drawable.dice4double;
+            case 5:
+                return R.drawable.dice5double;
+            case 6:
+                return R.drawable.dice6double;
+            default:
+                return R.drawable.dice1normal;
+        }
+    }
+
+    private int getDiceDrawableTriple(int tirada) {
+        switch (tirada) {
+            case 1:
+                return R.drawable.dice1double;
+            case 2:
+                return R.drawable.dice2double;
+            case 3:
+                return R.drawable.dice3double;
+            case 4:
+                return R.drawable.dice4double;
+            case 5:
+                return R.drawable.dice5double;
+            case 6:
+                return R.drawable.dice6double;
+            default:
+                return R.drawable.dice1normal;
+        }
+    }
+
     private void updateCounter() {
+        rolleado = false;
+
         puntosColumna[0] = getPuntosColumna(tablero, 0);
         puntosColumna[1] = getPuntosColumna(tablero, 1);
         puntosColumna[2] = getPuntosColumna(tablero, 2);
@@ -184,28 +226,58 @@ public class MainActivity2 extends AppCompatActivity {
         puntos_Jugador_columna_1.setText(String.valueOf(puntosColumna[1]));
         puntos_Jugador_columna_2.setText(String.valueOf(puntosColumna[2]));
 
-        int total = puntosColumna[0]+puntosColumna[1]+puntosColumna[2];
-
+        int total = puntosColumna[0] + puntosColumna[1] + puntosColumna[2];
         puntosJugador.setText(String.valueOf(total));
-
     }
 
     private int getPuntosColumna(int[][] tablero, int columna) {
         int puntos;
 
-        if (tablero[3][columna] == tablero[4][columna]&& tablero[4][columna] == tablero[5][columna]){
-            puntos = (tablero[3][columna] + tablero[4][columna] + tablero[5][columna])*3;
-        } else if(tablero[3][columna] == tablero[5][columna]){
+        if (tablero[3][columna] != 0 && tablero[3][columna] == tablero[4][columna] && tablero[4][columna] == tablero[5][columna]) {
+            puntos = (tablero[3][columna] + tablero[4][columna] + tablero[5][columna]) * 3;
+            getDiceDrawableFromPosition(3, columna).setImageResource(getDiceDrawableTriple(tablero[3][columna]));
+            getDiceDrawableFromPosition(4, columna).setImageResource(getDiceDrawableTriple(tablero[4][columna]));
+            getDiceDrawableFromPosition(5, columna).setImageResource(getDiceDrawableTriple(tablero[5][columna]));
+        } else if (tablero[3][columna] != 0 && tablero[3][columna] == tablero[5][columna]) {
             puntos = ((tablero[3][columna] + tablero[5][columna]) * 2) + tablero[4][columna];
-        }else if(tablero[4][columna] == tablero[5][columna]){
+            getDiceDrawableFromPosition(3, columna).setImageResource(getDiceDrawableDouble(tablero[3][columna]));
+            getDiceDrawableFromPosition(5, columna).setImageResource(getDiceDrawableDouble(tablero[5][columna]));
+        } else if (tablero[4][columna] != 0 && tablero[4][columna] == tablero[5][columna]) {
             puntos = ((tablero[4][columna] + tablero[5][columna]) * 2) + tablero[3][columna];
-        }else if(tablero[3][columna] == tablero[4][columna]) {
+            getDiceDrawableFromPosition(4, columna).setImageResource(getDiceDrawableDouble(tablero[4][columna]));
+            getDiceDrawableFromPosition(5, columna).setImageResource(getDiceDrawableDouble(tablero[5][columna]));
+        } else if (tablero[3][columna] != 0 && tablero[3][columna] == tablero[4][columna]) {
             puntos = ((tablero[3][columna] + tablero[4][columna]) * 2) + tablero[5][columna];
-        }else {
+            getDiceDrawableFromPosition(3, columna).setImageResource(getDiceDrawableDouble(tablero[3][columna]));
+            getDiceDrawableFromPosition(4, columna).setImageResource(getDiceDrawableDouble(tablero[4][columna]));
+        } else {
             puntos = tablero[3][columna] + tablero[4][columna] + tablero[5][columna];
         }
         return puntos;
-
     }
 
+    private ImageView getDiceDrawableFromPosition(int fila, int columna) {
+        if (fila == 3) {
+            if (columna == 0)
+                return dice_3_0;
+            else if (columna == 1)
+                return dice_3_1;
+            else
+                return dice_3_2;
+        } else if (fila == 4) {
+            if (columna == 0)
+                return dice_4_0;
+            else if (columna == 1)
+                return dice_4_1;
+            else
+                return dice_4_2;
+        } else {
+            if (columna == 0)
+                return dice_5_0;
+            else if (columna == 1)
+                return dice_5_1;
+            else
+                return dice_5_2;
+        }
+    }
 }
